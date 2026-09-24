452c30d3cdd9494efd8f33f92f0b10d8964a3926
m.ret <m.ret@rubika-edu.re>
2026-09-24 15:26:22 +0400

    flash light add v1 add

---
+++ Assets/script/flashlight.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class flashlight : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private GameObject torch;
+    [SerializeField] private float battery = 100f;
+    [SerializeField] private float batteryDepletionSpeed = 0.7f;
+    [SerializeField] private KeyCode SwtichKey = KeyCode.Q;
+    public GameObject player;
+    private bool on;
+
+    [Header("UI")]
+    [SerializeField] private Text batterytext;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(SwtichKey))
+            on = !on;
+
+        if (on && battery > 0)
+            battery -= batteryDepletionSpeed * Time.deltaTime;
+        else if (battery < 0.01f)
+            on = false;
+
+        batterytext.text = $"Battery: {(int)battery}";
+
+        torch.SetActive(on);
+
+        //player death
+        if (on && battery > 0)
+            battery -= batteryDepletionSpeed * Time.deltaTime;
+        else if (battery < 0.01f)
+
+            // to modify  for full game (disable les controles du joueur
+            Destroy(player.gameObject); 
+
+    }
+}
