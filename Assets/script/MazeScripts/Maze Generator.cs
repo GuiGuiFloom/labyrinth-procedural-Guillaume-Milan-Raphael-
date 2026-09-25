d7150798e6000c6cf1873e78af7ab80ebc7b7493
Guillaume Acquier <g.acquier@rubika-edu.re>
2026-09-25 10:37:41 +0400

    flashlight implementation

--- Assets/script/MazeScripts/Maze Generator.cs
+++ Assets/script/MazeScripts/Maze Generator.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+
+
+
+//. Globalement ce code permet de creer une grosse case de block, l'algo va detecter les blocks autour de lui et va decider de le desaciver ou non en fontion de si ils ont deja ete visité, il explore chaque possibilité dont ils disposent (dans ce cas un labyrinthe)
+// ce code est insipiré de l'algorithme recursive backtracker 
+
 public class MazeGenerator : MonoBehaviour
 {
 
